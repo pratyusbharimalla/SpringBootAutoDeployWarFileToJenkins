b6b537521c8740fffed80de22bcd6edfdc4f5c86
pratyusb <pratyus.j2ee@gmail.com>
2018-12-26 22:46:58 +0530

    first commit

---
+++ src/main/java/com/regency/jenkins/autodeploy/war/app/SpringBootAutoDeployWarFileToJenkins/SpringBootAutoDeployWarFileToJenkinsApplication.java
@@ -0,0 +1,14 @@
+package com.regency.jenkins.autodeploy.war.app.SpringBootAutoDeployWarFileToJenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAutoDeployWarFileToJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAutoDeployWarFileToJenkinsApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/regency/jenkins/autodeploy/war/app/SpringBootAutoDeployWarFileToJenkins/SpringBootAutoDeployWarFileToJenkinsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.regency.jenkins.autodeploy.war.app.SpringBootAutoDeployWarFileToJenkins;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootAutoDeployWarFileToJenkinsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
